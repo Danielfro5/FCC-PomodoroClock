c4582a47863fc22b199335cbdf3513003e2796c9
Danielfro5 <br3akcor3@yandex.com>
2017-05-26 12:30:00 +1000

    all basic functionality complete

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+var state = "Session";
+var stateSwitch = 1;
+var mainM=25;
+var pomoM=2;
+var m=mainM;
+var s=1;
+
 window.onload = function(){
   document.getElementById("mainTime").innerHTML = mainM;
   document.getElementById("pomoTime").innerHTML = pomoM;
@@ -15,7 +22,7 @@ function mainTime(){
     if (stateSwitch===1){
       state="BreakTime";
       stateSwitch=0;
-      m=pomoM;
+      m=document.getElementById("Time").innerHTML;
       s=0;
     }
     else if (stateSwitch===0){
@@ -31,26 +38,23 @@ function mainTime(){
 
 }
 
+function theMinus(id){
+  if(parseInt(document.getElementById(id).innerHTML)>0){
+  number = parseInt(document.getElementById(id).innerHTML)- 1;
+  document.getElementById(id).innerHTML = number;
+}else if(parseInt(document.getElementById(id).innerHTML)<=0){document.getElementById(id).innerHTML = 1;}}
 
-var state = "Session";
-var stateSwitch = 1;
-var mainM=25;
-var pomoM=2;
-var m=mainM;
-var s=1;
+function thePlus(id){
+  if(parseInt(document.getElementById(id).innerHTML)>0){
+  number = parseInt(document.getElementById(id).innerHTML)+ 1;
+  document.getElementById(id).innerHTML = number;
+}else if(parseInt(document.getElementById(id).innerHTML)<=0){document.getElementById(id).innerHTML = 1;}}
+
+function reset(){
+  m = document.getElementById("mainTime").innerHTML;
+  s = 0;
+  document.getElementById("showTime").innerHTML = state+"-"+m+":"+s;
+}
 
 setInterval(mainTime, 1000);
-
-function theMinus(id, number){
-  number = document.getElementById(id).innerHTML;
-  number -= 1;
-  document.getElementById(id).innerHTML = number;
-}
-function thePlus(id, number){
-  number = document.getElementById(id).innerHTML;
-  number += 1;
-  document.getElementById(id).innerHTML = number;
-}
-
-
-//nest global variables inside function before finished
+//nest global variables inside function before finished, setinterval reset timer with RESET function
