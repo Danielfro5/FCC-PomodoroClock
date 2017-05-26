51accfebff77da6dbc64aebb28d273e0adb8cf40
Danielfro5 <br3akcor3@yandex.com>
2017-05-26 10:47:44 +1000

    basic functionality and gui

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+window.onload = function(){
+  document.getElementById("mainTime").innerHTML = mainM;
+  document.getElementById("pomoTime").innerHTML = pomoM;
 
+};
 
 function mainTime(){
   s-=1;
@@ -11,27 +15,42 @@ function mainTime(){
     if (stateSwitch===1){
       state="BreakTime";
       stateSwitch=0;
-      m=pomoM-1;
+      m=pomoM;
+      s=0;
     }
     else if (stateSwitch===0){
       state="Session";
       stateSwitch=1;
-      m=mainM-1;
+      m=mainM;
+      s=0;
+
     }
   }
 
-  console.log(state+"-"+m+":"+s);
+  document.getElementById("showTime").innerHTML = state+"-"+m+":"+s;
 
 }
 
 
 var state = "Session";
 var stateSwitch = 1;
-var mainM=1;
+var mainM=25;
 var pomoM=2;
 var m=mainM;
-var s=0;
+var s=1;
 
 setInterval(mainTime, 1000);
 
-//remove global variables before finished
+function theMinus(id, number){
+  number = document.getElementById(id).innerHTML;
+  number -= 1;
+  document.getElementById(id).innerHTML = number;
+}
+function thePlus(id, number){
+  number = document.getElementById(id).innerHTML;
+  number += 1;
+  document.getElementById(id).innerHTML = number;
+}
+
+
+//nest global variables inside function before finished
